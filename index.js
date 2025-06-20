9bc5480473d6845a8b3894062ddd0da02d2b2915
Navis Ochieng <navis.ochieng@student.moringaschool.com>
2025-06-20 10:43:59 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+    const data = {
+        name: name,
+        email: email
+    }
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+    return fetch("http://localhost:3000/users", requestOptions)
+        .then(res => res.json())
+        .then(data => {
+            const newObject = document.createElement("div")
+            newObject.textContent = data.id
+            document.querySelector("body").appendChild(newObject)
+        })
+        .catch(error => {
+            const newObject = document.createElement("div")
+            newObject.textContent = error.message
+            document.querySelector("body").appendChild(newObject)
+        })
+}
+
+document.addEventListener("DOMContentLoaded", submitData("Navis", "navisayara@gmail.com"))
